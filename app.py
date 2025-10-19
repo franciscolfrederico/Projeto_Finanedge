ae4705ce725b37e9bdae62907def0653bb0f74e4
franciscoflfrederico <franciscoflfrederico@alu.ufc.br>
2025-10-19 08:50:01 -0300

    Força novo deploy no Vercel

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from datetime import timedelta
 
+
 app = Flask(__name__)
 app.secret_key = "troque-por-uma-chave-segura"
 app.permanent_session_lifetime = timedelta(hours=8)
